2985a3348fa2ec96b6b1a5fd226abf6d2596ca05
viniciusmatos08 <viniciusmatos08@gmail.com>
2020-08-12 12:49:40 -0300

    Commit inicial - projeto ja em andamento

---
+++ Modulo 1/apocalipse-zumbi-alura/Assets/ControlaJogador.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+
+    public float Velocidade = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+
+        transform.Translate(direcao * Velocidade * Time.deltaTime);
+
+        if(direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
+    }  
+}
